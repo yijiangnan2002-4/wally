f0354bb8d44671e79598bcca42dce1287938dafa
richard.luo <richard.luo@eastech.com>
2024-03-07 14:40:55 +0800

    modified charging case buttons function according to new spec.
    Signed-off-by: richard.luo <richard.luo@eastech.com>

--- mcu/project/ab158x/apps/bt_audio_common/src/apps/app_customer_commom/app_customer_common_activity.c
+++ mcu/project/ab158x/apps/bt_audio_common/src/apps/app_customer_commom/app_customer_common_activity.c
@@ -258,9 +258,19 @@ uint8_t Is_earbuds_agent_proc(void)
 void key_volumeup_proc(void)
 {
 	uint16_t *p_key_action = (uint16_t *)pvPortMalloc(sizeof(uint16_t)); // free by ui shell
-    	if (p_key_action)
+	*p_key_action = KEY_ACTION_INVALID;
+	apps_config_state_t app_mmi_state = apps_config_key_get_mmi_state();
+	if(app_mmi_state == APP_A2DP_PLAYING || app_mmi_state == APP_ULTRA_LOW_LATENCY_PLAYING || app_mmi_state == APP_WIRED_MUSIC_PLAY)
 	{
 		*p_key_action = KEY_VOICE_UP;
+	}
+	else
+	{
+		*p_key_action = KEY_HEARING_AID_VOLUME_UP;
+	}
+
+    	if (p_key_action)
+	{
 		ui_shell_send_event(false, EVENT_PRIORITY_HIGH, EVENT_GROUP_UI_SHELL_KEY, INVALID_KEY_EVENT_ID, p_key_action, sizeof(uint16_t), NULL, 50);
     	}
     	else
@@ -272,9 +282,19 @@ void key_volumeup_proc(void)
 void key_volumedown_proc(void)
 {
 	uint16_t *p_key_action = (uint16_t *)pvPortMalloc(sizeof(uint16_t)); // free by ui shell
-    	if (p_key_action)
+	*p_key_action = KEY_ACTION_INVALID;
+	apps_config_state_t app_mmi_state = apps_config_key_get_mmi_state();
+	if(app_mmi_state == APP_A2DP_PLAYING || app_mmi_state == APP_ULTRA_LOW_LATENCY_PLAYING || app_mmi_state == APP_WIRED_MUSIC_PLAY)
 	{
 		*p_key_action = KEY_VOICE_DN;
+	}
+	else
+	{
+		*p_key_action = KEY_HEARING_AID_VOLUME_DOWN;
+	}
+
+    	if (p_key_action)
+	{
 		ui_shell_send_event(false, EVENT_PRIORITY_HIGH, EVENT_GROUP_UI_SHELL_KEY, INVALID_KEY_EVENT_ID, p_key_action, sizeof(uint16_t), NULL, 50);
     	}
     	else
@@ -286,9 +306,25 @@ void key_volumedown_proc(void)
 void key_avrcp_next_proc(void)
 {
 	uint16_t *p_key_action = (uint16_t *)pvPortMalloc(sizeof(uint16_t)); // free by ui shell
-    	if (p_key_action)
+	*p_key_action = KEY_ACTION_INVALID;
+	apps_config_state_t app_mmi_state = apps_config_key_get_mmi_state();
+	if(app_mmi_state == APP_HFP_INCOMING)
+	{
+		*p_key_action = KEY_REJCALL;
+	}
+	else if(app_mmi_state == APP_HFP_CALLACTIVE || app_mmi_state == APP_HFP_CALLACTIVE_WITHOUT_SCO || app_mmi_state == APP_HFP_MULTITPART_CALL
+			||app_mmi_state == APP_HFP_OUTGOING || app_mmi_state == APP_STATE_HELD_ACTIVE || app_mmi_state == APP_HFP_TWC_OUTGOING)
+	{
+		*p_key_action = KEY_END_CALL;
+	}
+	else
 	{
 		*p_key_action = KEY_AVRCP_FORWARD;
+	}
+
+
+    	if (p_key_action)
+	{
 		ui_shell_send_event(false, EVENT_PRIORITY_HIGH, EVENT_GROUP_UI_SHELL_KEY, INVALID_KEY_EVENT_ID, p_key_action, sizeof(uint16_t), NULL, 50);
     	}
     	else
@@ -333,15 +369,6 @@ bool key_multifunction_short_click()
 	if(app_mmi_state == APP_HFP_INCOMING)
 	{
 		*p_key_action = KEY_ACCEPT_CALL;
-	}
-	else if(app_mmi_state == APP_HFP_TWC_INCOMING)
-	{
-		*p_key_action = KEY_3WAY_HOLD_ACTIVE_ACCEPT_OTHER;
-	}
-	else if(app_mmi_state == APP_HFP_CALLACTIVE || app_mmi_state == APP_HFP_CALLACTIVE_WITHOUT_SCO || app_mmi_state == APP_HFP_MULTITPART_CALL
-			||app_mmi_state == APP_HFP_OUTGOING || app_mmi_state == APP_STATE_HELD_ACTIVE || app_mmi_state == APP_HFP_TWC_OUTGOING)
-	{
-		*p_key_action = KEY_END_CALL;
 	}
 	else if(app_mmi_state == APP_A2DP_PLAYING || app_mmi_state == APP_ULTRA_LOW_LATENCY_PLAYING || app_mmi_state == APP_WIRED_MUSIC_PLAY)
 	{
